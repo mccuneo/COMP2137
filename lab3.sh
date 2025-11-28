3e94b1f1a025a0c575eacf8b11db2f45110025be
makayla <mccuneo2004@gmail.com>
2025-11-28 17:18:29 -0500

    Assignment 3 step 3 modified

--- lab3.sh
+++ lab3.sh
@@ -19,3 +19,31 @@ while [ $# -gt 0 ]; do
 	esac
 done
 
+move_excecute() {
+	local server="$1"
+	local name="$2"
+	local ip="$3"
+	local hostentryname="$4"
+	local hostentryipa="$5"
+	local verboseopt=" "
+	[ "$VERBOSE" = true ] && verboseopt="-VERBOSE"
+	echo "configure-host.sh is transferring to $server"
+	scp configure-host.sh remoteadmin@"$server":/root || { option_message "Eroor: SCP failed to $server"; exit 1; }
+	echo " configure-host.sh is executing on $server"
+	ssh remoteadmin@"$server" "/root/configure-host.sh -name $name  -ip $ip -hostentry $hostentryname $hostentryipa $verboseopt" 
+		|| { option_message "Error: configure-host.sh has failed on $server; exit 1; }
+}
+
+#Do update on server1
+move_excecute "server1-mgmt" "loghost" 192.168.16.3" "webhost" "192.168.16.4"
+#Do update on server2
+move_xecute "server2-mgmt" "webhost" "192.168.16.4" "loghost" "192.168.16.3"
+
+#Do update local /etc/hosts
+[ "$VERBOSE" =  true ] && verboseopt="-VERBOSE"
+./configure-host.sh -hostentry loghost 192.168.16.3 $verboseopt || { option_message "There is an error updating local hosts (loghost)"; exit 1; }
+./configure-host.sh -hostentry webhost 192.168.16.4 $verbose_option || { log_message "There was an error updating local hosts (webhost)"; exit 1; }
+
+echo "Deployment has been complete"
+
+	
